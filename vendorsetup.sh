be8029302e5159b60a659892ee395cf6d4f3ab8e
CuriousNom <aryanbiswas7585@gmail.com>
2024-05-08 13:01:27 +0530

    violet: Update repos
    * Following things are updated
      - Track Ancient kernel
      - Track Evox vendor
      - Update to latest Clang v18.0.1

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 # Cloning vendor and kernel repositories
-git clone --depth=1 https://github.com/ProjectBlaze-Devices/vendor_xiaomi_violet.git -b 14.0 vendor/xiaomi/violet
-git clone --depth=1 https://github.com/ProjectBlaze-Devices/chaste_kernel_xiaomi_violet.git -b 14.0 kernel/xiaomi/violet
+git clone --depth=1 https://github.com/Evolution-X-Devices/vendor_xiaomi_violet.git vendor/xiaomi/violet
+git clone --depth=1 https://github.com/Ancient-Project/android_kernel_xiaomi_violet.git kernel/xiaomi/violet
 
 # Cloning latest china firmware repository
 git clone --depth=1 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware/violet
@@ -9,4 +9,4 @@ git clone --depth=1 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-
 git clone --depth=1 https://gitlab.com/crdroidandroid/android_vendor_miuicamera -b 14.0-violet vendor/MiuiCamera
 
 # Cloning prebuilt Clang repository
-git clone --depth=1 https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r510928.git -b 14.0 prebuilts/clang/host/linux-x86/clang-r510928
+git clone --depth=1 https://gitlab.com/kei-space/clang/r522817.git prebuilts/clang/host/linux-x86/clang-r522817
