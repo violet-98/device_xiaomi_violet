5affb9ebb4f99ad185af2932b6de9e4f207ddc8a
Aryan Biswas <152459091+CuriousNom@users.noreply.github.com>
2024-05-11 23:13:24 +0530

    violet: Switch to perf kernel
    * Ancient kernel not booting for now

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 # Cloning vendor and kernel repositories
 git clone --depth=1 https://github.com/Evolution-X-Devices/vendor_xiaomi_violet.git vendor/xiaomi/violet
-git clone --depth=1 https://github.com/Ancient-Project/android_kernel_xiaomi_violet.git kernel/xiaomi/violet
+git clone --depth=1 https://github.com/kibria5/kernel_xiaomi_violet.git kernel/xiaomi/violet
 
 # Cloning latest china firmware repository
 git clone --depth=1 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware/violet
