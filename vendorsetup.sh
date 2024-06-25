a7d8aac782cc349c3090a3b0016f64e796a199e6
Aditya Pratap Singh <87426352+afterallafk@users.noreply.github.com>
2024-06-25 13:58:08 +0530

    violet: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,4 +11,4 @@ git clone --depth=1 https://gitlab.com/Joker-V2/android_vendor_MiuiCamera.git ve
 # Setup Neutron Clang
 mkdir prebuilts/clang/host/linux-x86/clang-neutron; cd prebuilts/clang/host/linux-x86/clang-neutron;
 curl -LO "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman";
-chmod +x antman; ./antman -S=05012024; ./antman --patch=glibc; cd ../../../../../;sed -i '/CLANG_EXTRA_FLAGS += --hip-path=\/dev\/null/d' vendor/lineage/build/tasks/kernel.mk;
+chmod +x antman; ./antman -S=05012024; ./antman --patch=glibc; cd ../../../../../;sed -i '/CLANG_EXTRA_FLAGS += --hip-path=\/dev\/null/d' vendor/blaze/build/tasks/kernel.mk;
